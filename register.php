ef18f6e66d9d844da4dc5aef42e90df67c396af1
sachinraghult <sachinraghult2002@gmail.com>
2021-05-08 15:20:42 +0530

    register form done

--- register.php
+++ register.php
@@ -83,26 +83,26 @@ button:hover {
 ?>
 
 <form action="/action_page.php" style="border:1px solid #ccc">
-  <div class="container content" style="width: 30%; height: 50%; margin:auto; margin-top: 80px; background-color:ivory">
+  <div class="container content" style="width: 500px; height: 50%; margin:auto; margin-top: 40px; background-color:ivory">
     <h1>Sign Up</h1>
     <p>Please fill in this form to create an account.</p>
     <hr>
 
+    <label for="name"><b>Name</b></label>
+    <input type="text" placeholder="Name" name="name" required>
+
     <label for="email"><b>Email</b></label>
-    <input type="text" placeholder="Enter Email" name="email" required>
+    <input type="text" placeholder="Email" name="email" required>
 
-    <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
+    <label for="pwd"><b>Password</b></label>
+    <input type="password" placeholder="Password" name="pwd" required>
 
-    <label for="psw-repeat"><b>Repeat Password</b></label>
-    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+    <label for="cnfm-pwd"><b>Confirm Password</b></label>
+    <input type="password" placeholder="Confirm Password" name="cnfm-pwd" required>
     
-    <label>
-      <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
-    </label>
-    
-    <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
-
+    <label for="profile"><b>Profile pic</b></label>
+    <input type="file" placeholder="profile" name="profile" required>
+    <br><br>
     <div class="clearfix">
       <button type="button" class="cancelbtn">Cancel</button>
       <button type="submit" class="signupbtn">Sign Up</button>
