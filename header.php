be1d7bd1a30f86d1b52b9a23e7e521bd9f57bd94
Sanjeev K M <er.sanjeev.au@gmail.com>
2021-05-08 12:45:34 +0530

    final sidenav

--- header.php
+++ header.php
@@ -4,13 +4,9 @@
   <title><?php $title?></title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <link href="css/sidenav.css" rel="stylesheet" type="text/css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-
 
 
 <div class="sidebar">
--- index.php
+++ index.php
@@ -3,9 +3,9 @@
     include("header.php");
 ?>
 
-<div class="content"">
+<div class="content">
   <h3>Right Aligned Navbar</h3>
   <p>The .navbar-right class is used to right-align navigation bar buttons.</p>
-</div>
+
 
 <?php include("footer.php"); ?>
