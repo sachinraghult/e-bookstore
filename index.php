4e725d3d984bc726056b03f8d07c25f30e3d906c
Sanjeev K M <er.sanjeev.au@gmail.com>
2021-05-08 15:33:23 +0530

    added bookcards

--- index.php
+++ index.php
@@ -1,11 +1,84 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Form</title>
+    <link rel="stylesheet" type="text/css" href="css/cards.css">
+</head>
+<body>
+
 <?php 
     $title = "Welcome";
     include("header.php");
 ?>
 
 <div class="content">
-  <h3>Right Aligned Navbar</h3>
-  <p>The .navbar-right class is used to right-align navigation bar buttons.</p>
+    <div class="wrapper">
+    <div class="cards_wrap">
+        <div class="card_item">
+        <div class="card_inner">
+            <div class="card_top">
+            <img src="https://i.imgur.com/qhE9KtV.jpg" alt="car" />
+            </div>
+            <div class="card_bottom">
+            <div class="card_category">
+                Travel
+            </div>
+            <div class="card_info">
+                <p class="title">10 Best Things about Travel</p>
+                <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ab.
+                </p>
+            </div>
+            <div class="card_creator">
+                By Alex Kato
+            </div>
+            </div>
+        </div>
+        </div>
+
+        <div class="card_item">
+        <div class="card_inner">
+            <div class="card_top">
+            <img src="https://i.imgur.com/qhE9KtV.jpg" alt="car" />
+            </div>
+            <div class="card_bottom">
+            <div class="card_category">
+                Travel
+            </div>
+            <div class="card_info">
+                <p class="title">10 Best Things about Travel</p>
+                <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ab.
+                </p>
+            </div>
+            <div class="card_creator">
+                By Alex Kato
+            </div>
+            </div>
+        </div>
+        </div>
+
+        <div class="card_item">
+        <div class="card_inner">
+            <div class="card_top">
+            <img src="https://i.imgur.com/qhE9KtV.jpg" alt="car" />
+            </div>
+            <div class="card_bottom">
+            <div class="card_category">
+                Travel
+            </div>
+            <div class="card_info">
+                <p class="title">10 Best Things about Travel</p>
+                <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ab.
+                </p>
+            </div>
+            <div class="card_creator">
+                By Alex Kato
+            </div>
+            </div>
+        </div>
+        </div>
 
 
 <?php include("footer.php"); ?>
