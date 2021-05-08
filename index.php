0ed48c20288e686cb398d0c3775fe18eac89a480
Sanjeev K M <er.sanjeev.au@gmail.com>
2021-05-08 10:18:35 +0530

    inc navbar

--- index.php
+++ index.php
@@ -1,25 +1,8 @@
-<<<<<<< HEAD
 <?php 
     $title = "Welcome";
     include("header.php");
 ?>
 
-
+<h1>Hello world</h1>
 
 <?php include("footer.php"); ?>
-=======
-<html>
-<head>
-<title>Hello World</title>
-</head>
-<body bgcolor="teal">
-    <h1>Hello World Boiis<h1>
-    <p style="color:blue; text-align:justify">
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reprehenderit sapiente error ratione. Cumque quos in aspernatur quod quae? Laborum, cumque? Iusto dolorum dolorem omnis odio laboriosam. Debitis quas omnis, a doloremque maiores consectetur illo rem fugiat quos deserunt alias odit. Eum cupiditate voluptatem at dicta quam fugiat labore quis expedita fuga dignissimos sapiente ab, enim esse autem facilis laudantium repellat quibusdam rerum laboriosam quia illo praesentium neque aperiam dolor! Pariatur, rerum repellendus obcaecati fuga molestiae dicta neque reprehenderit, odio veniam, nostrum placeat. Ea repudiandae voluptatem distinctio quas velit possimus! Aspernatur adipisci illo cumque ad at non rerum fugiat fugit dolores!
-    </p>
-    <div style="color: crimson; text-align:justify">
-    Hello World
-    </div>
-</body>
-</html>
->>>>>>> d8b4e73c88f8f7e9a40a87237f1b68ab0380e13e
