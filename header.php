cd350ae9f3c8490cb4cf134f3e8959a10339ef31
sachinraghult <sachinraghult2002@gmail.com>
2021-05-08 14:30:36 +0530

    This is final

--- header.php
+++ header.php
@@ -5,14 +5,21 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="css/sidenav.css" rel="stylesheet" type="text/css">
+
+  <style>
+  body{
+    background-image: url(images/library.jpg);
+  }
+  </style>
+
 </head>
 <body>
 
-<div style="position: sticky; top:0; display:block; padding:16px; background-color:black; color:white;text-align:center">E-BOOKSTORE</div>
+<div style="position: sticky; top:0; display:block; padding:16px; background-color: #ff6600; color:white;text-align:center">E-BOOKSTORE</div>
 
 <div class="sidebar">
-  <a class="active" href="#home">Home</a>
-  <a href="#news">News</a>
+  <a class="active" href="login.php">Login</a>
+  <a href="register.php">Register</a>
   <a href="#contact">Contact</a>
   <a href="#about">About</a>
 </div>
---
+++ login.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login Form</title>
+    <link rel="stylesheet" type="text/css" href="css/login.css">
+</head>
+<body>
+
+<?php 
+    $title = "Welcome";
+    include("header.php");
+?>
+
+
+   <div class="login-container content">
+       <img src="assets/avatar.jpg" alt="avatar image">
+       <form action="">
+           <h1>LOGIN</h1>
+           <div>
+               <label>Username</label>
+               <input type="text" name="username" placeholder="Enter your username here" value="">
+           </div>
+           <div>
+               <label>Password</label>
+               <input type="password" name="password" placeholder="Enter your password here" value="">
+           </div>
+           <input type="submit" name="login" value="LOGIN">
+           <a href="#">Forgot Password?</a>
+           <a href="#">Forgot Username?</a>
+       </form>
+   </div>
+
+<?php include("footer.php"); ?>
+    
+</body>
+</html>
