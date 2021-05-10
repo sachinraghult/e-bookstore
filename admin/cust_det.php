b09fa38aca29876bbdc72f1d0efcabedc57741d1
sachinraghult <sachinraghult2002@gmail.com>
2021-05-10 16:24:51 +0530

    Check

--- admin/cust_det.php
+++ admin/cust_det.php
@@ -1,3 +1,11 @@
+<?php
+  session_start();
+  include("../db.php");
+  if(!isset($_SESSION["AID"])){
+    header("location:../admin_login.php");
+  }
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,75 +18,52 @@
     include("header.php");
 ?>
 <h1 style="color: blue; text-align:center">Customer details</h1>
-<div class="content">
-    <div class="wrapper">
-    <div class="cards_wrap">
-        <div class="card_item">
-        <div class="card_inner">
-            <div class="card_top">
-            <img src="https://i.imgur.com/qhE9KtV.jpg" alt="car" />
-            </div>
-            <div class="card_bottom">
-            <div class="card_category">
-                Travel
-            </div>
-            <div class="card_info">
-                <p class="title">10 Best Things about Travel</p>
-                <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ab.
-                </p>
-            </div>
-            <div class="card_creator">
-                By Alex Kato
-            </div>
-            </div>
-        </div>
-        </div>
+<?php
 
-        <div class="card_item">
-        <div class="card_inner">
-            <div class="card_top">
-            <img src="https://i.imgur.com/qhE9KtV.jpg" alt="car" />
-            </div>
-            <div class="card_bottom">
-            <div class="card_category">
-                Travel
-            </div>
-            <div class="card_info">
-                <p class="title">10 Best Things about Travel</p>
-                <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ab.
-                </p>
-            </div>
-            <div class="card_creator">
-                By Alex Kato
-            </div>
-            </div>
-        </div>
-        </div>
+    $sql = "SELECT * FROM CUSTOMER";
+    $res = $db->query($sql);
 
-        <div class="card_item">
-        <div class="card_inner">
-            <div class="card_top">
-            <img src="https://i.imgur.com/qhE9KtV.jpg" alt="car" />
-            </div>
-            <div class="card_bottom">
-            <div class="card_category">
-                Travel
-            </div>
-            <div class="card_info">
-                <p class="title">10 Best Things about Travel</p>
-                <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ab.
-                </p>
-            </div>
-            <div class="card_creator">
-                By Alex Kato
-            </div>
-            </div>
-            
+    if($res->num_rows>0)
+    {
+        while($rows=$res->fetch_assoc())
+        {
+            echo '
+            <div class="wrapper">
+            <div class="cards_wrap">
+                <div class="card_item">
+                <div class="card_inner">
+                    <div class="card_top">
+                    <img src="https://i.imgur.com/qhE9KtV.jpg" alt="car" />
+                    </div>
+                    <div class="card_bottom">
+                    <div class="card_category">
+                        Travel
+                    </div>
+                    <div class="card_info">
+                        <p class="title">10 Best Things about Travel</p>
+                        <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ab.
+                        </p>
+                    </div>
+                    <div class="card_creator">
+                        By Alex Kato
+                    </div>
+                    </div>
+                </div>
+                </div>
+                ';
+        }
+        echo "
         </div>
         </div>
+        ";
+    }
+    else
+    {
+        echo "<p style='color: red'>No Records found</p>";
+    }
+
+?>
 
     </div>
     </body>
