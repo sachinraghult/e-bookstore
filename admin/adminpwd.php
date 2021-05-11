81e7245f5b70e1cb7eaf12303eed31f51982a030
sachinraghult <sachinraghult2002@gmail.com>
2021-05-11 07:36:57 +0530

    adminpwd

--- admin/adminpwd.php
+++ admin/adminpwd.php
@@ -11,6 +11,25 @@
 <head>
     <link rel="stylesheet" type="text/css" href="../css/register.css">
     <title>Change password</title>
+
+    <script type="text/javascript">
+    
+      function check() 
+      {
+        if (document.getElementById('newpwd').value ==
+        document.getElementById('cnfmpwd').value) 
+        {
+          document.getElementById('message').style.color = 'green';
+          document.getElementById('message').innerHTML = 'matching';
+        } 
+        else 
+        {
+          document.getElementById('message').style.color = 'red';
+          document.getElementById('message').innerHTML = 'not matching';
+        }
+}
+    </script>
+
 </head>
 <body>
 
@@ -21,23 +40,33 @@
     <h1>Change Password</h1>
     <hr>
     <?php
-      if (isset($_POST["reset"])) {
+      if (isset($_POST["reset"])) 
+      {
         $sql1 = "SELECT * FROM admin where apass = '{$_POST['oldpwd']}'";
         $res1 = $db->query($sql1);
-        if ($res1->num_rows == 0) {
+        if ($res1->num_rows == 0) 
+        {
           echo "<p style='color:red;'>Incorrect old password</p>";
         }
-        else if ($_POST["newpwd"] != $_POST["cnfm-pwd"]) {
-          echo "<p style='color:red;'>Passwords do not match</p>";
+        else if ($_POST["newpwd"] != $_POST["cnfmpwd"]) 
+        {
+          echo "<p style='color:red;'>Enter confirm password properly !</p>";
         }
-        else {
+        else if ($_POST["oldpwd"] == $_POST["newpwd"]) 
+        {
+          echo "<p style='color:red;'>New password should not be same !</p>";
+        }
+        else 
+        {
           $sql = "UPDATE admin set apass = '{$_POST['newpwd']}' where aid = '{$_SESSION['AID']}'";
           $res = $db->query($sql);
-          if ($res) {
-            echo "<p style='color:green;'>Password Updated</p>";
+          if ($res) 
+          {
+            echo "<p style='color:green;'>Password Updated Successfully</p>";
           }
-          else {
-            echo "<p style='color:red;'>Password Update failed</p>";
+          else 
+          {
+            echo "<p style='color:red;'>Password Update failed !</p>";
           }
         }
       }
@@ -47,10 +76,12 @@
     <input type="password" placeholder="Password" name="oldpwd" required>
 
     <label for="newpwd"><b>New Password</b></label>
-    <input type="password" placeholder="Password" name="newpwd" required>
+    <input type="password" placeholder="Password" name="newpwd" id="newpwd" required>
 
-    <label for="cnfm-pwd"><b>Confirm New Password</b></label>
-    <input type="password" placeholder="Confirm New Password" name="cnfm-pwd" required>
+    <label for="cnfmpwd"><b>Confirm New Password</b></label>
+    <input type="password" placeholder="Confirm New Password" name="cnfmpwd" id="cnfmpwd" onkeyup="check();" required>
+
+    <i><span id="message"></span></i><br><br>
 
     <div class="clearfix" style="padding-left: 3px">
       <button type="submit" class="resetpwd" name="reset">Reset Password</button>
