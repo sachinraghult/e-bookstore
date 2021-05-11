fe6ebf464331c2bda14b5443d366aadb528fea93
Sanjeev K M <er.sanjeev.au@gmail.com>
2021-05-11 21:44:45 +0530

    cust_book_det done

--- cust_book_det.php
+++ cust_book_det.php
@@ -1,5 +1,8 @@
 <?php
   session_start();
+  if(!isset($_SESSION["CUS_ID"])){
+    header("location:login.php");
+  }
   include("db.php");
 ?>
 
@@ -22,7 +25,7 @@
         include("includes/header.php");
     }
 
-    $sql = "SELECT book.*,category.cat_name from book inner join category on book.cat_id = category.cat_id where book.bid = {$_GET['id']};";
+    $sql = "SELECT book.*, category.cat_name from book inner join category on book.cat_id = category.cat_id where book.bid = {$_GET['id']};";
     $sql1 = "SELECT comment.comment, comment.logs, customer.cus_name from comment inner join customer where comment.cus_id = customer.cus_id and comment.bid = {$_GET['id']}
     ORDER BY comment.com_id DESC;";
     $sql2 = "SELECT * from payments where cus_id = {$_SESSION["CUS_ID"]} and bid = {$_GET["id"]}";
@@ -96,7 +99,7 @@
     }
     ?>
 
-    <form action='<?php echo $_SERVER['PHP_SELF'];?>' method='post' style='border:1px solid #ccc'>
+    <form action='<?php echo $_SERVER['REQUEST_URI'];?>' method='post' style='border:1px solid #ccc'>
     <div class='container content' style='width: 500px; height: 50%; margin:auto; margin-top: 40px; background-color:ivory'>
         <h1>Add Comment</h1>
         <hr>
@@ -107,6 +110,10 @@
             if(isset($_POST["submit"])){
                 $sql3 = "INSERT into comment (cus_id, bid, comment, logs) values ({$_SESSION["CUS_ID"]}, {$_GET["id"]}, '{$_POST["comment"]}', now())";
                 $res3 = $db->query($sql3);
+                if($res){
+                    echo "<p style='color:green'>Comment added</p>";
+                    header("location:{$_SERVER['REQUEST_URI']}");
+                }
             }
                 echo"
                 <label for='name'><b>Name</b></label>
@@ -132,10 +139,11 @@
                     <td>{$rows1['cus_name']}</td>
                     <td>{$rows1['comment']}</td>
                     <td>{$rows1['logs']}</td>
-                </tr>
-                </table>
-            </div>";
+                </tr>";
         }
+        echo "  
+            </table>
+            </div>";
     }
     else {
         echo "<p style='color: red'>No Comments</p>";
--- search_books.php
+++ search_books.php
@@ -1,3 +1,8 @@
+<?php
+  session_start();
+  include("db.php");
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
