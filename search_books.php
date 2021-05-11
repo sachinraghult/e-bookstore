607d80714d2b7ed147782745e4007ee1875b4d43
sachinraghult <sachinraghult2002@gmail.com>
2021-05-11 22:54:58 +0530

    Book search done

--- search_books.php
+++ search_books.php
@@ -6,75 +6,88 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <title>View Books</title>
     <link rel="stylesheet" type="text/css" href="css/cards.css">
     <link rel="stylesheet" type="text/css" href="css/register.css">
-    <title>Search Books</title>
-    <style>
-    body{
-        background-image: url(images/library.jpg);
-        background-repeat: no-repeat;
-        background-size: cover;
-    }
-    </style>
 </head>
 <body>
 
 <?php 
-    if(isset($_SESSION['CUS_ID']))
-    {
-        include("includes/header.php");
-    }
-    else
-    {
-        include("includes/main_header.php");
-    }
+    include("includes/header.php");
 ?>
-
-<form action="/action_page.php" style="border:1px solid #ccc">
+<h1 style="color: blue; text-align:center">All Books</h1>
+<form action="<?php echo $_SERVER["PHP_SELF"];?>" method = "post" style="border:1px solid #ccc">
   <div class="container content" style="width: 500px; height: 50%; margin:auto; margin-top: 40px; background-color:ivory">
     <h1>Search books</h1>
     <hr>
 
+    <label for="searchby">Search by&ensp;&ensp;</label>
+    <select name="searchby" style="background-color: rgb(235, 235, 235);width:80%;height:40px;" >
+          <option value="book.bname">Name</option>
+          <option value="book.author">Author</option>
+          <option value="category.cat_name">Category</option>
+          <option value="book.keywords">Keywords</option>
+    </select>
+    <br><br>
+
     <label for="name"><b>Name</b></label>
-    <input type="text" placeholder="Name" name="name" required>
+    <input type="text" placeholder="Enter name of book or author" name="name">
     <br>
-    <div class="clearfix" style="padding-left: 3px">
-      <button type="submit" class="signup">Search</button>
+    <div class="clearfix" style="padding-left: 3px"> 
+      <button type="submit" class="signup" name="search" style="width: 48%;">Search</button>
+      <a href="<?php echo $_SERVER["PHP_SELF"];?>"><button class="signup" style="background-color: red;width:48%;float:right;">Clear</button></a>
     </div>
   </div>
 </form>
-
-<br>
-<h1 style="text-align:center; color:blue">Results</h1>
-
-<div class="content">
-    <div class="wrapper">
-    <div class="cards_wrap">
-        <div class="card_item">
-        <div class="card_inner">
-            <div class="card_top">
-            <img src="https://i.imgur.com/qhE9KtV.jpg" alt="car" />
-            </div>
-            <div class="card_bottom">
-            <div class="card_category">
-                Travel
-            </div>
-            <div class="card_info">
-                <p class="title">10 Best Things about Travel</p>
-                <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, ab.
-                </p>
-            </div>
-            <div class="card_creator">
-                By Alex Kato
-            </div>
-            </div>
+<?php
+    if (!isset($_POST["search"])) {
+        $sql = "SELECT book.*, category.* from book inner join category where book.cat_id = category.cat_id";
+    }
+    else {
+        $sql = "SELECT book.*, category.* from book inner join category where book.cat_id = category.cat_id and {$_POST['searchby']} LIKE '%{$_POST['name']}%'";
+    }
+    $res = $db->query($sql);
+    if($res->num_rows>0)
+    {
+        echo'<div class="wrapper">
+             <div class="cards_wrap">';
+            while($rows=$res->fetch_assoc())
+            {
+                echo "
+                    <div class='card_item'>
+                    <div class='card_inner'>
+                        <div class='card_top'>
+                            <img src={$rows["bimage"]} alt='profile' />
+                        </div>
+                        <div class='card_bottom'>
+                        <div class='card_info'>
+                            <p class='title'>{$rows["bname"]}</p>
+                            <p>
+                            {$rows['author']}
+                            </p>
+                            <p>
+                            {$rows['cat_name']}
+                            </p>
+                        </div>
+                        <div class='card_creator'>
+                        <a href='login.php'><button>Pay</button></a>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                    ";
+            }
+        echo "
         </div>
         </div>
-    </div>
-    </div>
+        ";
+    }
+    else
+    {
+        echo "<p style='color: red'>No Records found</p>";
+    }
 
-<?php include("includes/footer.php"); ?>
-
-</body>
-</html>
+?>
+    </div>
+    </body>
+    </html>
