0a81d80d4e852bcb8e1a4f67426b9f96a1a692ef
sachinraghult <sachinraghult2002@gmail.com>
2021-05-11 18:36:40 +0530

    user page done

--- books.php
+++ books.php
@@ -1,8 +1,5 @@
 <?php
   session_start();
-  if(!isset($_SESSION["CUS_ID"])){
-    header("location:index.php");
-  }
   include("db.php");
 ?>
 
@@ -11,6 +8,7 @@
 <head>
     <title>Login Form</title>
     <link rel="stylesheet" type="text/css" href="css/cards.css">
+    <link rel="stylesheet" type="text/css" href="css/register.css">
     <style>
         body{
         background-image: url(images/library.jpg);
@@ -26,6 +24,54 @@
 ?>
 
     <?php
+
+
+    if(!isset($_SESSION["CUS_ID"]))
+    {
+        $sql="SELECT * from book where book.cat_id={$_GET['id']};";
+        $res=$db->query($sql);
+        echo "<br><br><h3>Available Books :</h3>";
+        
+        if($res->num_rows>0)
+        {
+            echo '
+            <div class="wrapper">
+            <div class="cards_wrap">
+            ';
+            while($rows=$res->fetch_assoc())
+            {
+                echo "
+                <div class='card_item'>
+                <div class='card_inner'>
+                    <div class'card_top'>
+                    <img src='admin/{$rows['bimage']}' alt='image' />
+                    </div>
+                    <div class='card_bottom'>
+                    <div class='card_info'>
+                        <p class='title'>{$rows['bname']}</p>
+                        <p>{$rows['author']}</p>
+                    </div>
+                    <div class='card_creator'>
+                        <a href='a.php?id={$rows['bid']}'><button>PAY</button></a>
+                    </div>
+                    </div>
+                </div>
+                </div>            
+                ";
+            }
+            echo '
+            </div>
+            </div>
+            ';
+        }
+        else
+        {
+            echo "<p style='color: red'>No Books found  :(</p>";
+        }
+
+    }
+    else
+    {
         $sql1="SELECT * from category where category.cat_id={$_GET["id"]};";
         $res1=$db->query($sql1);
         $rows1=$res1->fetch_assoc();
@@ -36,12 +82,13 @@
         $res2=$db->query($sql2);
 
         echo"<h3>BOOKS PURCHASED UNDER {$rows1['cat_name']} :</h3><br>";
-        echo '
+        
+        if($res2->num_rows>0)
+        {
+            echo '
             <div class="wrapper">
             <div class="cards_wrap">
             ';
-        if($res2->num_rows>0)
-        {
             while($rows2=$res2->fetch_assoc())
             {
                 echo '
@@ -71,7 +118,7 @@
         }
         else
         {
-            echo "<p style='color: red'>You did'nt purchase any book in {$rows1['cat_name']}</p>";
+            echo "<p style='color: red'>You did'nt purchase any book in {$rows1['cat_name']}  :(</p>";
         }
 
         $sql3="SELECT * from book
@@ -79,35 +126,34 @@
                and book.cat_id={$_GET['id']};";
         $res3=$db->query($sql3);
 
+        echo "<br><br><h3>RECOMENDATIONS :</h3>";
         
-       // echo "                                                                                                                         ";
-        echo "<p><h3>RECOMENDATIONS :</h3></p>";
-        echo '
+        if($res3->num_rows>0)
+        {
+            echo '
             <div class="wrapper">
             <div class="cards_wrap">
             ';
-        if($res3->num_rows>0)
-        {
             while($rows3=$res3->fetch_assoc())
             {
-                echo '
-                <div class="card_item">
-                <div class="card_inner">
-                    <div class="card_top">
-                    <img src="admin/{$rows3["bimage"]}"" alt="image" />
+                echo "
+                <div class='card_item'>
+                <div class='card_inner'>
+                    <div class'card_top'>
+                    <img src='admin/{$rows3['bimage']}' alt='image' />
                     </div>
-                    <div class="card_bottom">
-                    <div class="card_info">
-                        <p class="title">{$rows3["bname"]}</p>
-                        <p>{$rows3["author"]}</p>
+                    <div class='card_bottom'>
+                    <div class='card_info'>
+                        <p class='title'>{$rows3['bname']}</p>
+                        <p>{$rows3['author']}</p>
                     </div>
-                    <div class="card_creator">
-                        <a href="#.php?id={$rows3["bid"]}"><button>PAY</button></a>
+                    <div class='card_creator'>
+                        <a href='a.php?id={$rows3['bid']}'><button>PAY</button></a>
                     </div>
                     </div>
                 </div>
                 </div>            
-                ';
+                ";
             }
             echo '
             </div>
@@ -118,6 +164,7 @@
         {
             echo "<p style='color: red'>No Books found  :(</p>";
         }
+    }
     ?>
 
 
