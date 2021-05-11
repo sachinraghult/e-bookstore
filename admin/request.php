1004135c5e77879f4d70d070d58c1d310c0e4798
srivatsavr02 <srivatsavr02@gmail.com>
2021-05-11 13:52:37 +0530

    search

--- admin/request.php
+++ admin/request.php
@@ -18,18 +18,48 @@
 <head>
     <title>Admin Home</title>
     <link rel="stylesheet" type="text/css" href="../css/tables.css">
+    <link rel="stylesheet" type="text/css" href="../css/register.css">
 </head>
 <body>
 <?php 
     include("header.php");
 ?>
+<h3 style='text-align: center;'>CUSTOMER REQUESTS</h3>
+<form action="<?php echo $_SERVER["PHP_SELF"];?>" method = "post" style="border:1px solid #ccc">
+  <div class="container content" style="width: 500px; height: 50%; margin:auto; margin-top: 40px; background-color:ivory">
+    <h1>Search books</h1>
+    <hr>
+
+    <label for="searchby">Search by&ensp;&ensp;</label>
+    <select name="searchby" style="background-color: rgb(235, 235, 235);width:80%;height:40px;" >
+        <option value="request.bname">Book</option>   
+        <option value="customer.cus_name">Customer</option>
+        <option value="request.request">Request</option>
+    </select>
+    <br><br>
+
+    <label for="name"><b>Name</b></label>
+    <input type="text" placeholder="Enter name of book or author" name="name">
+    <br>
+    <div class="clearfix" style="padding-left: 3px"> 
+      <button type="submit" class="signup" name="search" style="width: 48%;">Search</button>
+      <a href="<?php echo $_SERVER["PHP_SELF"];?>"><button class="signup" style="background-color: red;width:48%;float:right;">Clear</button></a>
+    </div>
+  </div>
+</form>
 <?php
-    $sql="SELECT customer.cus_name, request.*
-    from request inner join customer
-    where customer.cus_id=request.cus_id;";
+    if (!isset($_POST['search'])) {
+        $sql="SELECT customer.cus_name, request.*
+        from request inner join customer
+        where customer.cus_id=request.cus_id;";
+    }
+    else {
+        $sql = "SELECT customer.cus_name, request.*
+        from request inner join customer
+        where customer.cus_id=request.cus_id and {$_POST['searchby']} LIKE '%{$_POST['name']}%';";
+    }
     $res=$db->query($sql);
 
-    echo "<h3 style='text-align: center;'>CUSTOMER REQUESTS</h3>";
     if($res->num_rows>0)
     {
         echo "
