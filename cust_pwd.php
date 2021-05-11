b08148b5b4295e994e5aec58d8d992887f2db54e
sachinraghult <sachinraghult2002@gmail.com>
2021-05-11 22:20:35 +0530

    Password and sql export

--- cust_pwd.php
+++ cust_pwd.php
@@ -1,41 +1,93 @@
+<?php
+  session_start();
+  if(!isset($_SESSION["CUS_ID"])){
+    header("location:index.php");
+  }
+  include("db.php");
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="css/register.css">
-    <style>
-    body{
-        background-image: url(images/library.jpg);
-        background-repeat: no-repeat;
-        background-size: cover;
-    }
-    </style>
+    <title>Change password</title>
+
+    <script type="text/javascript">
+    
+      function check() 
+      {
+        if (document.getElementById('newpwd').value ==
+        document.getElementById('cnfmpwd').value) 
+        {
+          document.getElementById('message').style.color = 'green';
+          document.getElementById('message').innerHTML = 'matching';
+        } 
+        else 
+        {
+          document.getElementById('message').style.color = 'red';
+          document.getElementById('message').innerHTML = 'not matching';
+        }
+      }
+    </script>
+
 </head>
 <body>
 
 <?php include("includes/header.php");?>
 
-<form action="/action_page.php" style="border:1px solid #ccc">
+<form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post" style="border:1px solid #ccc">
   <div class="container content" style="width: 500px; height: 50%; margin:auto; margin-top: 40px; background-color:ivory">
     <h1>Change Password</h1>
-    
     <hr>
+    <?php
+      if (isset($_POST["reset"])) 
+      {
+        $sql1 = "SELECT * FROM customer where cus_pass = '{$_POST['oldpwd']}'";
+        $res1 = $db->query($sql1);
+        if ($res1->num_rows == 0) 
+        {
+          echo "<p style='color:red;'>Incorrect old password</p>";
+        }
+        else if ($_POST["newpwd"] != $_POST["cnfmpwd"]) 
+        {
+          echo "<p style='color:red;'>Enter confirm password properly !</p>";
+        }
+        else if ($_POST["oldpwd"] == $_POST["newpwd"]) 
+        {
+          echo "<p style='color:red;'>New password should not be same !</p>";
+        }
+        else 
+        {
+          $sql = "UPDATE customer set cus_pass = '{$_POST['newpwd']}' where cus_id = '{$_SESSION['CUS_ID']}'";
+          $res = $db->query($sql);
+          if ($res) 
+          {
+            echo "<p style='color:green;'>Password Updated Successfully</p>";
+          }
+          else 
+          {
+            echo "<p style='color:red;'>Password Update failed !</p>";
+          }
+        }
+      }
+    ?>
 
     <label for="oldpwd"><b>Old Password</b></label>
     <input type="password" placeholder="Password" name="oldpwd" required>
 
     <label for="newpwd"><b>New Password</b></label>
-    <input type="password" placeholder="Password" name="newpwd" required>
+    <input type="password" placeholder="Password" name="newpwd" id="newpwd" onkeyup="check();" required>
 
-    <label for="cnfm-pwd"><b>Confirm New Password</b></label>
-    <input type="password" placeholder="Confirm New Password" name="cnfm-pwd" required>
+    <label for="cnfmpwd"><b>Confirm New Password</b></label>
+    <input type="password" placeholder="Confirm New Password" name="cnfmpwd" id="cnfmpwd" onkeyup="check();" required>
+
+    <i><span id="message"></span></i><br><br>
 
     <div class="clearfix" style="padding-left: 3px">
-      <button type="submit" class="resetpwd">Reset Password</button>
+      <button type="submit" class="resetpwd" name="reset">Reset Password</button>
     </div>
   </div>
 </form>
-
-<?php include("includes/footer.php"); ?>
 
 </body>
 </html>
--- ebookstore.sql
+++ ebookstore.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 10, 2021 at 08:42 PM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 8.0.5
+-- Generation Time: May 11, 2021 at 06:48 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.27
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -38,8 +39,174 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`aid`, `aname`, `apass`) VALUES
-(1, 'admin', 'admin'),
-(2, 'admin1', 'admin1');
+(1, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
+  `bid` int(11) NOT NULL,
+  `bname` varchar(250) NOT NULL,
+  `author` varchar(250) NOT NULL,
+  `bimage` varchar(250) NOT NULL,
+  `bfile` varchar(250) NOT NULL,
+  `keywords` varchar(250) NOT NULL,
+  `cat_id` int(11) NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`bid`, `bname`, `author`, `bimage`, `bfile`, `keywords`, `cat_id`, `price`) VALUES
+(1, 'hello world', 'sanjeev', 'image.png', 'book1.pdf', 'hello', 2, 200),
+(2, 'xampp', 'apache', 'bit.image', 'bit.pdf', 'bit', 4, 150),
+(3, 'PHP', 'Tutor Joes', 'media/book_img/Sachin.jpeg', 'media/book_file/OS_LAB_13.pdf', 'PHP Backend Webdesigning', 4, 450),
+(4, 'Aadhar', 'Modi', 'media/book_img/WhatsApp Image 2020-08-03 at 11.06.08 PM.jpeg', 'media/book_file/DBMS_LAB_1.pdf', 'India Swach Bharath', 1, 300),
+(5, 'abcdefg', 'xyz', 'media/book_img/Sachin.jpeg', 'media/book_file/Full-HD-Desktop-Wallpaper.jpg', 'lmnopq', 2, 500);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category`
+--
+
+CREATE TABLE `category` (
+  `cat_id` int(11) NOT NULL,
+  `cat_name` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `category`
+--
+
+INSERT INTO `category` (`cat_id`, `cat_name`) VALUES
+(1, 'MAGAZINES'),
+(2, 'COMICS'),
+(3, 'LITERATURE'),
+(4, 'SCIENCE'),
+(5, 'HORROR'),
+(6, 'THRILLER'),
+(7, 'STORY'),
+(8, 'SUMMA'),
+(9, 'SUMMA');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `com_id` int(11) NOT NULL,
+  `cus_id` int(11) NOT NULL,
+  `bid` int(11) NOT NULL,
+  `comment` varchar(250) NOT NULL,
+  `logs` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `comment`
+--
+
+INSERT INTO `comment` (`com_id`, `cus_id`, `bid`, `comment`, `logs`) VALUES
+(3, 2, 1, 'Very nice', '2021-05-27 03:26:20'),
+(4, 1, 2, 'Interesting sir!', '2021-05-10 20:38:28'),
+(5, 4, 2, 'Helloooo', '2021-05-11 21:32:41'),
+(6, 4, 2, 'Hii', '2021-05-11 21:49:46'),
+(7, 4, 2, 'He he he', '2021-05-11 21:49:55');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `cus_id` int(11) NOT NULL,
+  `cus_name` varchar(250) NOT NULL,
+  `cus_image` varchar(250) NOT NULL,
+  `cus_mail` varchar(250) NOT NULL,
+  `cus_pass` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `customer`
+--
+
+INSERT INTO `customer` (`cus_id`, `cus_name`, `cus_image`, `cus_mail`, `cus_pass`) VALUES
+(1, 'Hello', 'image.cd', 'cueifwhdius', 'sdadads'),
+(2, 'dasdas', 'sdasd.f', 'dsadsaf', 'dsdagf'),
+(4, 'SACHIN RAGHUL T', 'admin/media/profile_img/Sachin.jpeg', 'sachinraghult2002@gmail.com', 'sachin'),
+(5, 'kiruku', 'admin/media/profile_img/WhatsApp Image 2020-08-03 at 11.06.08 PM.jpeg', 'kiruku@gmail.com', 'kiruku'),
+(6, 'SACHIN RAGHUL T', 'admin/media/profile_img/Sachin.jpeg', 'sachinraghult2002@gmail.com', 'summa'),
+(7, 'srivatsav', 'admin/media/profile_img/WhatsApp Image 2020-08-03 at 11.06.08 PM.jpeg', 'srivatsav@gmail.com', 'athirai'),
+(8, 'sanjeev', 'admin/media/profile_img/WhatsApp Image 2020-08-03 at 11.05.36 PM.jpeg', 'er.sanjeev@gmail.com', 'bhaga'),
+(9, 'abcd', 'admin/media/profile_img/WhatsApp Image 2020-08-03 at 11.05.36 PM.jpeg', 'abcd@gmail.com', 'abcd'),
+(10, 'xyz', 'admin/media/profile_img/Sachin.jpeg', 'xyz@gmail.com', 'xyz');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payments`
+--
+
+CREATE TABLE `payments` (
+  `bill_id` int(11) NOT NULL,
+  `txn_id` varchar(250) NOT NULL,
+  `cus_id` int(11) NOT NULL,
+  `bid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `payments`
+--
+
+INSERT INTO `payments` (`bill_id`, `txn_id`, `cus_id`, `bid`) VALUES
+(1, 'dsdad', 1, 1),
+(2, 'dsfgg', 2, 1),
+(3, 'fsssd', 2, 2),
+(4, 'sadasd', 1, 1),
+(5, 'xxxxxx', 4, 2),
+(6, 'yyyyyyy', 6, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `request`
+--
+
+CREATE TABLE `request` (
+  `rid` int(11) NOT NULL,
+  `cus_id` int(11) NOT NULL,
+  `bname` varchar(250) NOT NULL,
+  `request` varchar(250) NOT NULL,
+  `logs` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `request`
+--
+
+INSERT INTO `request` (`rid`, `cus_id`, `bname`, `request`, `logs`) VALUES
+(3, 1, 'comp sci', 'I need urgently', '2021-05-18 07:23:33'),
+(4, 2, 'upsc books', 'i am an civil aspirant', '2021-05-08 10:28:28');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subs`
+--
+
+CREATE TABLE `subs` (
+  `subs_id` int(11) NOT NULL,
+  `cus_id` int(11) NOT NULL,
+  `bid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -52,6 +219,56 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`aid`);
 
 --
+-- Indexes for table `book`
+--
+ALTER TABLE `book`
+  ADD PRIMARY KEY (`bid`),
+  ADD KEY `cat_id` (`cat_id`);
+
+--
+-- Indexes for table `category`
+--
+ALTER TABLE `category`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`com_id`),
+  ADD KEY `cus_id` (`cus_id`),
+  ADD KEY `bid` (`bid`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`cus_id`);
+
+--
+-- Indexes for table `payments`
+--
+ALTER TABLE `payments`
+  ADD PRIMARY KEY (`bill_id`),
+  ADD KEY `cus_id` (`cus_id`),
+  ADD KEY `bid` (`bid`);
+
+--
+-- Indexes for table `request`
+--
+ALTER TABLE `request`
+  ADD PRIMARY KEY (`rid`),
+  ADD KEY `cus_id` (`cus_id`);
+
+--
+-- Indexes for table `subs`
+--
+ALTER TABLE `subs`
+  ADD PRIMARY KEY (`subs_id`),
+  ADD KEY `cus_id` (`cus_id`),
+  ADD KEY `bid` (`bid`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -60,6 +277,85 @@ ALTER TABLE `admin`
 --
 ALTER TABLE `admin`
   MODIFY `aid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `book`
+--
+ALTER TABLE `book`
+  MODIFY `bid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `category`
+--
+ALTER TABLE `category`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `com_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `customer`
+--
+ALTER TABLE `customer`
+  MODIFY `cus_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `payments`
+--
+ALTER TABLE `payments`
+  MODIFY `bill_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `request`
+--
+ALTER TABLE `request`
+  MODIFY `rid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `subs`
+--
+ALTER TABLE `subs`
+  MODIFY `subs_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `book`
+--
+ALTER TABLE `book`
+  ADD CONSTRAINT `book_ibfk_1` FOREIGN KEY (`cat_id`) REFERENCES `category` (`cat_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `comment`
+--
+ALTER TABLE `comment`
+  ADD CONSTRAINT `comment_ibfk_1` FOREIGN KEY (`cus_id`) REFERENCES `customer` (`cus_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `comment_ibfk_2` FOREIGN KEY (`bid`) REFERENCES `book` (`bid`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `payments`
+--
+ALTER TABLE `payments`
+  ADD CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`cus_id`) REFERENCES `customer` (`cus_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `payments_ibfk_2` FOREIGN KEY (`bid`) REFERENCES `book` (`bid`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `request`
+--
+ALTER TABLE `request`
+  ADD CONSTRAINT `request_ibfk_1` FOREIGN KEY (`cus_id`) REFERENCES `customer` (`cus_id`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `subs`
+--
+ALTER TABLE `subs`
+  ADD CONSTRAINT `subs_ibfk_1` FOREIGN KEY (`cus_id`) REFERENCES `customer` (`cus_id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `subs_ibfk_2` FOREIGN KEY (`bid`) REFERENCES `book` (`bid`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
