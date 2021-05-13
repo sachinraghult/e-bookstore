1ddc26c8207524463dac363ea2737e1b4c00421b
Sanjeev K M <er.sanjeev.au@gmail.com>
2021-05-13 20:47:10 +0530

    changes in navbar

--- profile.php
+++ profile.php
@@ -11,8 +11,8 @@
 <html>
 <head>
     
-    <link rel="stylesheet" type="text/css" href="css/profile.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/profile.css">
 
     <style>
         body{
