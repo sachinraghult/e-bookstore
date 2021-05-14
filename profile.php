3e0a9eae2ec8ac0cf3a175fa2f018977ae819ada
srivatsavr02 <srivatsavr02@gmail.com>
2021-05-14 16:48:56 +0530

    profile

--- profile.php
+++ profile.php
@@ -12,7 +12,7 @@
 <head>
     
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="css/profile.css">
+    <link rel="stylesheet" type="text/css" href="css/profile.scss">
 
     <style>
         body{
@@ -27,17 +27,26 @@
 
 <h2 style="text-align:center; margin-top:10%">User Profile Card</h2>
 
+<script>
+document.querySelectorAll('.button').forEach(button => button.innerHTML = '<div><span>' + button.textContent.trim().split('').join('</span><span>') + '</span></div>');
+</script>
+
 <?php
   $sql = "SELECT * from customer where customer.cus_id={$_SESSION["CUS_ID"]}";
   $res = $db->query($sql);
   $row = $res->fetch_assoc();
   echo"
-    <div class='card'>
-      <img src='{$row['cus_image']}' alt='profile' style='width:100%'>
-      <h1>{$row['cus_name']}</h1>
-      <p>{$row['cus_mail']}</p>
-      <p><a href='cust_transaction.php'><button style='margin-bottom: 0%;'>View Transactions</button></a></p>
-    </div>";
+  <div class='container'>
+    <div class='row'>
+    <div class='col-lg-4'>
+      <div class='card p-0'>
+      <div class='card-image'><img src='{$row['cus_image']}' alt='profile' style='width:100%'></div>
+      <div class='card-content d-flex flex-column align-items-center' style='color:black'><b>
+      <h4 class='pt-2'>{$row['cus_name']}</h4>
+      <h5>{$row['cus_mail']}</h5</b><br><br>
+      <ul> <li><div>
+      <p><a href='cust_transaction.php' class='rainbow rainbow-1' style='border-radius:30px;text-decoration:none'>View Transactions</a></p></li></ul><div>
+    </div></div></div></div>";
 ?>
 
 </body>
