47fe82dbfb4cc2988d7e6689c51703af16c6dd70
sachinraghult <sachinraghult2002@gmail.com>
2021-05-16 17:21:38 +0530

    Some changes

--- admin/book_det.php
+++ admin/book_det.php
@@ -14,8 +14,6 @@
     <title>View Books</title>
     
     <link rel="stylesheet" type="text/css" href="../css/tables.css">
-    <link rel="stylesheet" type="text/css" href="../css/book_det_card.css">
-   
 </head>
 <body>
 
@@ -26,43 +24,16 @@
     $res = $db->query($sql);
     $res1 = $db->query($sql1);
     $rows = $res->fetch_assoc();
+
     if($res->num_rows>0)
     {
+    $img = "{$rows['bimage']}";
     echo"
-        <style>
-        .scene:nth-child(1) .movie .poster {
-            background-image: url('{$rows["bimage"]}');
-        }
-        
-        .scene:nth-child(1) .info header {
-            background-image: url('{$rows["bimage"]}');
-        }
-        </style>
-        
-        <div class='container mt30'>
-        <div class='wrapper'>
-            <ul class='stage clearfix'>
-                <li class='scene'>
-                    <div class='movie' onlclick='returntrue'>
-                        <div class='poster'>
-                        </div>
-                        <div class='info'>
-                            <header>
-                                <h1>Heading</h1>
-                                <span class='year'>20015</span>
-                                <span class='rating'>PG</span>
-                                <span class='duration'>102 M</span>
-                            </header>
-                            <p>
-                                CakeCounter provides specially baked cakes and treats that are available for shipping around the Jamshedpur. Our custom baked cakes are made using fresh and wholesome ingredients and are created with skill and artistry. From brownies and cookies or the perfect strawberry cake, our bakery provides just the right items for celebratory moments or everyday treats.
-                            </p>
-                        </div>
-                    </div>
-                </li>
-            </ul>
-	    </div>
-        </div>
-        
+        <div class='thumb' style='text-size: 50px;'>
+        <a href='#'>
+            <span ><button class='glow-on-hover' type='button' >PAY</button></span>
+        </a>
+        </div>   
         ";
     }
     else {
@@ -102,5 +73,139 @@
     }
 ?>
 
+<style>
+    @import url(https://fonts.googleapis.com/css?family=Montserrat);
+/*basic reset*/
+* {margin: 0; padding: 0;}
+
+/*a nice BG*/
+body {
+	background: #544; /*fallback*/
+	background: linear-gradient(#544, #565);
+}
+
+/*Thumbnail Background*/
+.thumb {
+	width: 500px; height: 750px; margin: 70px auto;
+	perspective: 1000px;
+}
+.thumb a {
+	display: block; width: 100%; height: 100%;
+	/*double layered BG for lighting effect*/
+	background: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)),url(<?php echo $img; ?>) no-repeat center top;
+    
+    background-size: contain;
+	/*disabling the translucent black bg on the main image*/
+	background-size: 0, cover;
+	/*3d space for children*/
+	transform-style: preserve-3d;
+	transition: all 0.5s;
+}
+.thumb:hover a {transform: rotateX(80deg); transform-origin: bottom;}
+/*bottom surface */
+.thumb a:after {
+	/*36px high element positioned at the bottom of the image*/
+	content: ''; position: absolute; left: 0; bottom: 0; 
+	width: 100%; height: 36px;
+	/*inherit the main BG*/
+	background: inherit; background-size: cover, cover;
+	/*draw the BG bottom up*/
+	background-position: bottom;
+	/*rotate the surface 90deg on the bottom axis*/
+	transform: rotateX(90deg); transform-origin: bottom;
+    opacity: 0.65;
+}
+/*label style*/
+.thumb a span {
+	color: white; text-transform: uppercase;
+	position: absolute; top: 100%; left: 0; width: 100%;
+	font: bold 12px/36px Montserrat; text-align: center;
+	/*the rotation is a bit less than the bottom surface to avoid flickering*/
+	transform: rotateX(-89.99deg); transform-origin: top;
+	z-index: 1;
+}
+/*shadow*/
+.thumb a:before {
+	content: ''; position: absolute; top: 0; left: 0;
+	width: 100%; height: 100%;
+	background: rgba(0, 0, 0, 0.5); 
+	box-shadow: 0 0 100px 50px rgba(0, 0, 0, 0.5);
+	transition: all 0.5s; 
+	/*by default the shadow will be almost flat, very transparent, scaled down with a large blur*/
+	opacity: 0.15;
+	transform: rotateX(95deg) translateZ(-80px) scale(0.75);
+	transform-origin: bottom;
+}
+.thumb:hover a:before {
+	opacity: 1;
+	/*blurred effect using box shadow as filter: blur is not supported in all browsers*/
+	box-shadow: 0 0 25px 25px rgba(0, 0, 0, 0.5);
+	/*pushing the shadow down and scaling it down to size*/
+	transform: rotateX(0) translateZ(-60px) scale(0.85);
+}
+
+
+
+.glow-on-hover {
+    width: 500px;
+    height: 40px;
+    border: none;
+    outline: none;
+    color: #fff;
+    background: #111;
+    cursor: pointer;
+    position: relative;
+    z-index: 0;
+    border-radius: 10px;
+}
+
+.glow-on-hover:before {
+    content: '';
+    background: linear-gradient(45deg, #ff0000, #ff7300, #fffb00, #48ff00, #00ffd5, #002bff, #7a00ff, #ff00c8, #ff0000);
+    position: absolute;
+    top: -2px;
+    left:-2px;
+    background-size: 400%;
+    z-index: -1;
+    filter: blur(5px);
+    width: calc(100% + 4px);
+    height: calc(100% + 4px);
+    animation: glowing 20s linear infinite;
+    opacity: 0;
+    transition: opacity .3s ease-in-out;
+    border-radius: 10px;
+}
+
+.glow-on-hover:active {
+    color: #000
+}
+
+.glow-on-hover:active:after {
+    background: transparent;
+}
+
+.glow-on-hover:hover:before {
+    opacity: 1;
+}
+
+.glow-on-hover:after {
+    z-index: -1;
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background: #111;
+    left: 0;
+    top: 0;
+    border-radius: 10px;
+}
+
+@keyframes glowing {
+    0% { background-position: 0 0; }
+    50% { background-position: 400% 0; }
+    100% { background-position: 0 0; }
+}
+
+</style>
 </body>
 </html>
