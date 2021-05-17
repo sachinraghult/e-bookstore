3105364bd62e0cf4c707588709d8824cf4bf4cc9
srivatsavr02 <srivatsavr02@gmail.com>
2021-05-17 21:47:13 +0530

    blackboard form

--- admin/transaction.php
+++ admin/transaction.php
@@ -10,11 +10,137 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Admin Home</title>
+    <title>Customer Transactions</title>
     <link rel="stylesheet" type="text/css" href="../css/tables.css">
-    <link rel="stylesheet" type="text/css" href="../css/register.css">
     <link rel="stylesheet" type="text/css" href="../css/font.scss">
     
+    <style>
+       .blackboard {
+                position: relative;
+                width: 640px;
+                margin: 7% auto;
+                border: tan solid 12px;
+                border-top: #bda27e solid 12px;
+                border-left: #b19876 solid 12px;
+                border-bottom: #c9ad86 solid 12px;
+                box-shadow: 0px 0px 6px 5px rgba(58, 18, 13, 0), 0px 0px 0px 2px #c2a782, 0px 0px 0px 4px #a58e6f, 3px 4px 8px 5px rgba(0, 0, 0, 0.5);
+                background-image: radial-gradient( circle at left 30%, rgba(34, 34, 34, 0.3), rgba(34, 34, 34, 0.3) 80px, rgba(34, 34, 34, 0.5) 100px, rgba(51, 51, 51, 0.5) 160px, rgba(51, 51, 51, 0.5)), linear-gradient( 215deg, transparent, transparent 100px, #222 260px, #222 320px, transparent), radial-gradient( circle at right, #111, rgba(51, 51, 51, 1));
+                background-color: #333;
+        }
+
+        .blackboard:before {
+                box-sizing: border-box;
+                display: block;
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                background-image: linear-gradient( 175deg, transparent, transparent 40px, rgba(120, 120, 120, 0.1) 100px, rgba(120, 120, 120, 0.1) 110px, transparent 220px, transparent), linear-gradient( 200deg, transparent 80%, rgba(50, 50, 50, 0.3)), radial-gradient( ellipse at right bottom, transparent, transparent 200px, rgba(80, 80, 80, 0.1) 260px, rgba(80, 80, 80, 0.1) 320px, transparent 400px, transparent);
+                border: #2c2c2c solid 2px;
+                content: "Search Billing History";
+                font-family: 'Permanent Marker', cursive;
+                font-size: 2.2em;
+                color: rgba(238, 238, 238, 0.7);
+                text-align: center;
+                padding-top: 20px;
+        }
+
+        .form {
+                padding: 70px 20px 20px;
+        }
+
+        p {
+                position: relative;
+                margin-bottom: 1em;
+        }
+
+        span {
+            font-family: 'Permanent Marker', cursive;
+            font-size: 1.0em;
+            color: rgba(238, 238, 238, 0.7);
+        }
+
+        label {
+                vertical-align: middle;
+                font-family: 'Permanent Marker', cursive;
+                font-size: 1.6em;
+                color: rgba(238, 238, 238, 0.7);
+        }
+
+        p:nth-of-type(5) > label {
+                vertical-align: top;
+        }
+
+        input,
+        textarea {
+                vertical-align: middle;
+                padding-left: 10px;
+                background: none;
+                border: none;
+                font-family: 'Permanent Marker', cursive;
+                font-size: 1.6em;
+                color: rgba(238, 238, 238, 0.8);
+                line-height: .6em;
+                outline: none;
+        }
+
+        select {
+            vertical-align: middle;
+                padding-left: 10px;
+            background : transparent;
+                border: none;
+            width : 40%;
+                font-family: 'Permanent Marker', cursive;
+                font-size: 1.6em;
+                color: rgba(238, 238, 238, 0.8);
+                line-height: .6em;
+                outline: none;
+        }
+
+        option {
+            vertical-align: middle;
+                padding-left: 10px;
+            background : rgb(63, 62, 70);
+                border: none;
+                font-family: 'Permanent Marker', cursive;
+                font-size: 1.0em;
+                color: rgba(238, 238, 238, 0.8);
+                line-height: .6em;
+                outline: none;
+        }
+
+        textarea {
+            margin-top: 1%;
+                height: 120px;
+                font-size: 1.4em;
+                line-height: 1em;
+                resize: none;
+        }
+
+        input[type="submit"] {
+                cursor: pointer;
+                color: rgba(238, 238, 238, 0.7);
+                line-height: 1em;
+                padding: 0;
+        }
+
+        input[type="submit"]:focus {
+                background: rgba(238, 238, 238, 0.2);
+                color: rgba(238, 238, 238, 0.2);
+        }
+
+        ::-moz-selection {
+                background: rgba(238, 238, 238, 0.2);
+                color: rgba(238, 238, 238, 0.2);
+                text-shadow: none;
+        }
+
+        ::selection {
+                background: rgba(238, 238, 238, 0.4);
+                color: rgba(238, 238, 238, 0.3);
+                text-shadow: none;
+        }
+    </style>
+
 </head>
 <body>
 
@@ -28,30 +154,32 @@
 </div>
 
 <form action="<?php echo $_SERVER["PHP_SELF"];?>" method = "post">
-  <div class="container content" style="width: 500px; height: 50%; margin:auto; margin-top: 20px; background-color:ivory">
-    <h1>Search Billing History</h1>
+<div style="margin-right: 10%;">
+  <div class="blackboard">
+      <div class="form">
     <hr>
-
+    <p>
     <label for="searchby">Search by&ensp;&ensp;</label>
-    <select name="searchby" style="background-color: rgb(235, 235, 235);width:80%;height:40px;" >
+    <select name="searchby">
           <option value="book.bname">Book</option>
           <option value="customer.cus_name">Customer</option>
-    </select>
-    <br><br>
+    </select></p>
 
-    <label for="name"><b>Search</b></label>
-    <input type="text" placeholder="Enter name of book or customer" name="name">
-    <br><hr style="width:46%; float: left"><span style="text-align:center">&ensp;or&nbsp;</span><hr style="width:46%; float: right"><br><br>
-
-    <label for="pricebelow"><b>Price Range</b></label><br>
-    <input type="number" placeholder="Above" name="pricebelow" style="background-color: rgb(235, 235, 235);width:48%;height:40px; float:left">
-    <input type="number" placeholder="Below" name="priceabove" style="background-color: rgb(235, 235, 235);width:48%;height:40px; float:right">
-    <br><br><br>
-
-    <div class="clearfix" style="padding-left: 3px"> 
-      <button type="submit" class="signup" name="search" style="width: 48%;">Search</button>
-      <a href="<?php echo $_SERVER["PHP_SELF"];?>"><button class="signup" style="background-color: red;width:48%;float:right;">Clear</button></a>
-    </div>
+    <p>
+    <label for="name">Search&ensp;&ensp;&ensp;&ensp;&ensp;</label>
+    <input type="text" name="name" placeholder="Enter text to search" />
+    </p><br><hr style="width:45%; float: left"><span style="text-align:center">&ensp;or&nbsp;</span><hr style="width:49%; float: right"><br><br>
+    <p>
+    <label for="pricebelow">Price Range</label>&ensp;&ensp;
+    <input type="number" placeholder="Above" name="pricebelow" style="width:20%">&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;
+    <input type="number" placeholder="Below" name="priceabove" style="width:20%">
+    </p>
+        <br><br>
+    <p class="wipeout">&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;
+      <input type="submit" name="search" value="Search:-" />&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;
+      <input type="submit" value="Clear:-" />
+    </p>
+    </div></div>
   </div>
 </form>
 <br>
--- cust_request.php
+++ cust_request.php
@@ -1,4 +1,5 @@
 <?php
+ob_start();
   session_start();
   if(!isset($_SESSION["CUS_ID"])){
     header("location:login.php");
@@ -24,19 +25,138 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>View Books</title>
+    <title>Request books</title>
     <link rel="stylesheet" type="text/css" href="css/cards.css">
     <link rel="stylesheet" type="text/css" href="css/tables.css">
-    <link rel="stylesheet" type="text/css" href="css/register.css">
+
+    <style>
+       .blackboard {
+                position: relative;
+                width: 640px;
+                margin: 7% auto;
+                border: tan solid 12px;
+                border-top: #bda27e solid 12px;
+                border-left: #b19876 solid 12px;
+                border-bottom: #c9ad86 solid 12px;
+                box-shadow: 0px 0px 6px 5px rgba(58, 18, 13, 0), 0px 0px 0px 2px #c2a782, 0px 0px 0px 4px #a58e6f, 3px 4px 8px 5px rgba(0, 0, 0, 0.5);
+                background-image: radial-gradient( circle at left 30%, rgba(34, 34, 34, 0.3), rgba(34, 34, 34, 0.3) 80px, rgba(34, 34, 34, 0.5) 100px, rgba(51, 51, 51, 0.5) 160px, rgba(51, 51, 51, 0.5)), linear-gradient( 215deg, transparent, transparent 100px, #222 260px, #222 320px, transparent), radial-gradient( circle at right, #111, rgba(51, 51, 51, 1));
+                background-color: #333;
+        }
+
+        .blackboard:before {
+                box-sizing: border-box;
+                display: block;
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                background-image: linear-gradient( 175deg, transparent, transparent 40px, rgba(120, 120, 120, 0.1) 100px, rgba(120, 120, 120, 0.1) 110px, transparent 220px, transparent), linear-gradient( 200deg, transparent 80%, rgba(50, 50, 50, 0.3)), radial-gradient( ellipse at right bottom, transparent, transparent 200px, rgba(80, 80, 80, 0.1) 260px, rgba(80, 80, 80, 0.1) 320px, transparent 400px, transparent);
+                border: #2c2c2c solid 2px;
+                content: "Add Request";
+                font-family: 'Permanent Marker', cursive;
+                font-size: 2.2em;
+                color: rgba(238, 238, 238, 0.7);
+                text-align: center;
+                padding-top: 20px;
+        }
+
+        .form {
+                padding: 70px 20px 20px;
+        }
+
+        p {
+                position: relative;
+                margin-bottom: 1em;
+        }
+
+        label {
+                vertical-align: middle;
+                font-family: 'Permanent Marker', cursive;
+                font-size: 1.6em;
+                color: rgba(238, 238, 238, 0.7);
+        }
+
+        p:nth-of-type(5) > label {
+                vertical-align: top;
+        }
+
+        input,
+        textarea {
+                vertical-align: middle;
+                padding-left: 10px;
+                background: none;
+                border: none;
+                font-family: 'Permanent Marker', cursive;
+                font-size: 1.6em;
+                color: rgba(238, 238, 238, 0.8);
+                line-height: .6em;
+                outline: none;
+        }
+
+
+        select {
+            vertical-align: middle;
+                padding-left: 10px;
+            background : transparent;
+                border: none;
+            width : 40%;
+                font-family: 'Permanent Marker', cursive;
+                font-size: 1.6em;
+                color: rgba(238, 238, 238, 0.8);
+                line-height: .6em;
+                outline: none;
+        }
+
+        option {
+            vertical-align: middle;
+                padding-left: 10px;
+            background : rgb(63, 62, 70);
+                border: none;
+                font-family: 'Permanent Marker', cursive;
+                font-size: 1.0em;
+                color: rgba(238, 238, 238, 0.8);
+                line-height: .6em;
+                outline: none;
+        }
+
+        textarea {
+            margin-top: 1%;
+                height: 120px;
+                font-size: 1.4em;
+                line-height: 1em;
+                resize: none;
+        }
+
+        input[type="submit"] {
+                cursor: pointer;
+                color: rgba(238, 238, 238, 0.7);
+                line-height: 1em;
+                padding: 0;
+        }
+
+        input[type="submit"]:focus {
+                background: rgba(238, 238, 238, 0.2);
+                color: rgba(238, 238, 238, 0.2);
+        }
+
+        ::-moz-selection {
+                background: rgba(238, 238, 238, 0.2);
+                color: rgba(238, 238, 238, 0.2);
+                text-shadow: none;
+        }
+
+        ::selection {
+                background: rgba(238, 238, 238, 0.4);
+                color: rgba(238, 238, 238, 0.3);
+                text-shadow: none;
+        }
+    </style>
 </head>
 <body>
 
 
 <form action="<?php echo $_SERVER["PHP_SELF"];?>"  method = "post">
-  <div class="container content" style="width: 500px; height: 50%; margin:auto; margin-top: 40px; background-color:ivory">
-    <br>
-    <h1>Add Request</h1>
-    <br>
+  <div class="blackboard">
+      <div class="form">
     <hr>
 
     <?php
@@ -53,19 +173,16 @@
             }
         }
     ?>
-
-    <label for="bname"><b>Name Of the Book</b></label>
-    <input type="text" placeholder="Enter the name of book" name="bname" required>
-
-    <label for="request"><b>Request</b></label>
-    <br>
-    <div style="padding-top:7px">
-    <textarea name="request" placeholder="Enter your request" rows="3" cols="40" style="background-color : rgb(243, 243, 243);" required></textarea>
-    </div>
-    
-    <br>
-    <div class="clearfix" style="padding-left: 3px">
-      <button type="submit" class="submit" name="submit">Submit</button>
+    <p><br>
+    <label for="bname">Book Name</label>&ensp;&ensp;
+    <input type="text" placeholder="Enter text" name="bname" required>
+    </p>
+    <p>
+    <label for="request" style="vertical-align:top;">Request</label>&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;&ensp;
+    <textarea name="request" placeholder="Enter your request" required></textarea>
+    </p>
+    <p class="wipeout" style="text-align:center">
+      <input type="submit" class="submit" name="submit" value="Submit" /></p>
     </div>
   </div>
 </form>
@@ -112,6 +229,6 @@
             echo "<p style='color: red'>No requests</p>";
         }
     ?>
-
+<?php include("includes/footer.php"); ob_end_flush();?>
     </body>
 </html>
