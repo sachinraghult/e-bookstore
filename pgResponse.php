bcb3ee015dadaf07b2ee19ef4bd163fe29f544d2
sachinraghult <sachinraghult2002@gmail.com>
2021-05-20 08:00:32 +0530

    Success

--- pgResponse.php
+++ pgResponse.php
@@ -17,8 +17,17 @@ $paytmChecksum = isset($_POST["CHECKSUMHASH"]) ? $_POST["CHECKSUMHASH"] : ""; //
 
 //Verify all parameters received from Paytm pg to your application. Like MID received from paytm pg is same as your application�s MID, TXN_AMOUNT and ORDER_ID are same as what was sent by you to Paytm PG for initiating transaction etc.
 $isValidChecksum = verifychecksum_e($paramList, PAYTM_MERCHANT_KEY, $paytmChecksum); //will return TRUE or FALSE string.
+?>
 
-echo "<body style='background-color: #eedfcc;'>";
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Register</title>
+    <link rel="stylesheet" type="text/css" href="css/pay_table.scss">
+</head>
+<body style="margin-top: 0%">
+
+<?php
 
 if($isValidChecksum == "TRUE") {
     if ($_POST["STATUS"] == "TXN_SUCCESS") {
@@ -44,46 +53,47 @@ if($isValidChecksum == "TRUE") {
             $res2 = $db->query($sql2);
 
             if($res1){
-                echo "<br><h1 style='text-align:center; color:green;'>Transaction successful</h1>" . "<br/>";
+                echo "<div class='aa_htmlTable'><h2 class='aa_h2' style='text-align:center; color:green;'>Transaction Successful</h2>";
             }else{
-                echo "<br><h1 style='text-align:center; color:red;'>Transaction status failure</h1>" . "<br/>";
+                echo "<div class='aa_htmlTable'><h2 class='aa_h2' style='text-align:center; color:red;'>Transaction status failure</h2>";
             }
         }
         
     }
     else {
-        echo "<br><h1 style='text-align:center; color:red;'>Transaction status failure</h1>" . "<br/>";
+        echo "<div class='aa_htmlTable'><h2 class='aa_h2' style='text-align:center; color:red;'>Transaction status failure</h2>";
     }
 
     if (isset($_POST) && count($_POST)>0 )
     {
-        echo "<table style='float: center; margin: auto; border: 1px solid black; border-collapse: collapse; width : 50%'>";
+        echo "<table><tbody>";
         foreach($_POST as $paramName => $paramValue) 
         {
             if(($paramName == 'MID') || ($paramName == 'TXNID') || ($paramName == 'RESPCODE') || ($paramName == 'CHECKSUMHASH'))
                 continue;
             else
-                echo "<tr><td style='border: .5px solid black; padding: 10px;'>" . $paramName . "</td><td style='border: .5px solid black; padding: 10px;'>" . $paramValue . "</td></tr>";
+                echo "<tr><th>" . $paramName . "</th><td>" . $paramValue . "</td></tr>";
         }
-        echo "</table>";
+        
         echo "
-        <table style='float: center; margin: auto;'>
-        <tr>
+        
+        <tr style='border-style: hidden'>
         <div style='display: inline;' align='center'><br><br>
-        <td><div><button onclick='window.print()' style='color: white; background-color: #4caf50; padding: 15px 32px; text-align: center; text-decoration: none; border-radius: 4px;font-size: 100%;''>Print</button></div></td>
+        <td><div><button onclick='window.print()' style='color: white; background-color: #00ff00; padding: 15px 32px; text-align: center; text-decoration: none; border-radius: 4px;font-size: 100%;''>Print</button></div></td>
         <td><div><a href='book_purchased.php'><button style='color: white; background-color: #0099ff; padding: 15px 32px; text-align: center; text-decoration: none; border-radius: 4px; font-size: 100%;'>Back</button></a></div></td>
         </div>
         </tr>
-        </table>
+        </tbody></table></div>
+        </body>
+        </html>
         ";
     }
     
 
 }
 else {
-    echo "<b>Checksum mismatched.</b>";
+    echo "<b>Checksum mismatched.</b></body></html>";
     //Process transaction as suspicious.
 }
 
 ?>
-
